3dbc9db5380912ffc732f103a313de308556f103
xmmmmmovo <13256172360@163.com>
2018-07-19 15:45:42 +0800

    更改loginActivity

--- app/src/main/java/com/example/a41448/huawu/view/fragment/LoginFragment.java
+++ app/src/main/java/com/example/a41448/huawu/view/fragment/LoginFragment.java
@@ -1,9 +1,15 @@
 package com.example.a41448.huawu.view.fragment;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -23,6 +29,7 @@ import com.dd.processbutton.iml.ActionProcessButton;
 import com.example.a41448.huawu.R;
 import com.example.a41448.huawu.base.BaseActivity;
 import com.example.a41448.huawu.utils.FragmentUtils;
+import com.example.a41448.huawu.view.activity.LoginActivity;
 import com.example.a41448.huawu.view.activity.MainActivity;
 
 public class LoginFragment extends Fragment{
@@ -63,18 +70,17 @@ public class LoginFragment extends Fragment{
                     //到主activity
                     MainActivity.startActivity(getActivity());
                 }else if (FirstLoginAccout && login()){
-                    fragmentReplace(new LableChosingFragment());
+                    FragmentUtils.replaceFragment(fragmentManager, new LableChosingFragment(), R.id.login_fragment_container);
                     //到职业选取跟tag选取activity
                 }else{
                     AlertDialog dialog = new AlertDialog.Builder(context)
                             .setTitle("登录错误")
                             .setCancelable(false)
                             .setMessage("未找到账号 请检查账号密码是否正确")
-                            .setNegativeButton("暂不注册",null)
+                            .setNegativeButton("暂不注册",null)//后期可加入游客系统
                             .setPositiveButton("注册账号", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
-                                    fragmentReplace(new RegisterFragment());//注册可用dialog代替
                                 }
                             })
                             .show();
@@ -83,9 +89,15 @@ public class LoginFragment extends Fragment{
         });
 
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View view) {
-                fragmentReplace(new RegisterFragment());
+                getActivity().getWindow().setExitTransition(null);
+                getActivity().getWindow().setEnterTransition(null);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(getActivity(),
+                        mRegisterButton, mRegisterButton.getTransitionName());
+                startActivity(new Intent(context, RegisterFragment.class), options.toBundle());
             }
         });
 
@@ -96,7 +108,7 @@ public class LoginFragment extends Fragment{
         mAccoutNumber = view.findViewById(R.id.et_username);
         mAccoutPassword = view.findViewById(R.id.et_password);
         cardView = view.findViewById(R.id.cv);
-        mRegisterButton = view.findViewById(R.id.fab);
+        mRegisterButton = view.findViewById(R.id.register_fab);
         mLoginButton = (ActionProcessButton)view.findViewById(R.id.login_button);
     }
 
@@ -106,10 +118,6 @@ public class LoginFragment extends Fragment{
     private boolean login() {
 
         return true;
-    }
-
-    private void fragmentReplace(Fragment fragment){
-        FragmentUtils.replaceFragment(fragmentManager, fragment, R.id.login_fragment_container);
     }
 
 }
