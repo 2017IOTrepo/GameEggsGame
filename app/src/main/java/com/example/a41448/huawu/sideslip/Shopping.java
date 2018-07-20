e77dbc1670fde473891896edb6b4fab97b35b78f
DreamMemory001 <982179530@qq.com>
2018-07-20 19:17:20 +0800

    load

---
+++ app/src/main/java/com/example/a41448/huawu/sideslip/Shopping.java
@@ -0,0 +1,4 @@
+package com.example.a41448.huawu.sideslip;
+
+public class Shopping {
+}
---
+++ app/src/main/java/com/example/a41448/huawu/sideslip_adapter/Achievements_Adapter.java
@@ -0,0 +1,4 @@
+package com.example.a41448.huawu.sideslip_adapter;
+
+public class Achievements_Adapter {
+}
