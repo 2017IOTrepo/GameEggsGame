3c89bf86b477fe12eb82614f4b56865799fa79ad
xmmmmmovo <13256172360@163.com>
2018-08-06 10:46:46 +0800

    merge branch

---
+++ app/src/main/java/com/example/a41448/huawu/view/sideslip/Label/Label_Main.java
@@ -0,0 +1,60 @@
+package com.example.a41448.huawu.view.sideslip.Label;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.a41448.huawu.R;
+import com.example.a41448.huawu.sideslip.Label.bean.SimpleTitleTip;
+import com.example.a41448.huawu.sideslip.Label.bean.Tip;
+import com.example.a41448.huawu.sideslip.Label.widget.TipItemView;
+
+import java.util.ArrayList;
+
+public class Label_Main   extends AppCompatActivity{
+    private EasyTipDragView easyTipDragView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        easyTipDragView = findViewById(R.id.easy_tip_drag_view);
+        //设置已经包含的标签数据
+        easyTipDragView.setAddData(TipDataModel.getAddTips());
+        //设置可以添加的标签数据
+        easyTipDragView.setDragData(TipDataModel.getDragTips());
+        //在easyTipDragView处于非编辑模式下点积item的毁掉（编辑模式下点积item作为删除的item）
+        easyTipDragView.setSelectedListener(new TipItemView.OnSelectedListener() {
+            @Override
+            public void onTileSelected(Tip entity, int position, View view) {
+                toast(((SimpleTitleTip) entity).getTip());
+            }
+        });
+        //设置每次数据改变之后的回调（每次标签删除和添加都会回调）
+        easyTipDragView.setDataResultCallback(new EasyTipDragView.OnDataChangeResultCallback() {
+            @Override
+            public void onDataChangeResult(ArrayList<Tip> tips) {
+                Log.i("skr", tips.toString());
+            }
+        });
+        //设置点击“确定”按钮最终数据的回调
+        easyTipDragView.setOnCompleteCallback(new EasyTipDragView.OnCompleteCallback() {
+            @Override
+            public void onComplete(ArrayList<Tip> tips) {
+                //     toast("最终数据：" + tips.toString());
+                //  for (int i = 0 ; i <tips.size();i++){
+                //      TipDataModel.dragTips[i] = String.valueOf(tips.get(i));
+                //  }
+
+            }
+        });
+        easyTipDragView.open();
+    }
+
+    public void toast(String str){
+        Toast.makeText(Label_Main.this, str, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/a41448/huawu/view/sideslip/Label/TipDataModel.java
@@ -0,0 +1,38 @@
+package com.example.a41448.huawu.view.sideslip.Label;
+
+import com.example.a41448.huawu.sideslip.Label.bean.SimpleTitleTip;
+import com.example.a41448.huawu.sideslip.Label.bean.Tip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Xuchao on 2018/7/26 0026.
+         */
+public class TipDataModel {
+    public  static String[] dragTips ={"宅男","运动狂","直播","体育","健身","科技","段子","漫画","手机控","爬山","长跑","孤独","外卖"
+            ,"游泳","旅游","网游"};
+    public static String[] addTips ={"二次元","颜控","自我","星族","自由","高冷","读书","外向","文艺","小清新","嘻哈","微博"};
+    public static List<Tip> getDragTips(){
+        List<Tip> result = new ArrayList<>();
+        for(int i=0;i<dragTips.length;i++){
+            String temp =dragTips[i];
+            SimpleTitleTip tip = new SimpleTitleTip();
+            tip.setTip(temp);
+            tip.setId(i);
+            result.add(tip);
+        }
+        return result;
+    }
+    public static List<Tip> getAddTips(){
+        List<Tip> result = new ArrayList<>();
+        for(int i=0;i<addTips.length;i++){
+            String temp =addTips[i];
+            SimpleTitleTip tip = new SimpleTitleTip();
+            tip.setTip(temp);
+            tip.setId(i+dragTips.length);
+            result.add(tip);
+        }
+        return result;
+    }
+}
